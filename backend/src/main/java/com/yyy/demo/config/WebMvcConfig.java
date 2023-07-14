48db51c46fafeab7bf35874fe6974d39349b9280
Yuyan Yang <yxqnyyy@gmail.com>
2023-07-14 15:58:14 +0200

    fix the package path

--- backend/src/main/java/com/yyy/demo/config/WebMvcConfig.java
+++ backend/src/main/java/com/yyy/demo/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package backend.com.yyy.demo.config;
+package com.yyy.demo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
